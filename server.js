f1d143df678fb1c6a26e8cf26c856b4b6827ce15
viniciusdev772 <viniciusdev37@gmail.com>
2023-07-27 20:28:38 -0300

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
-const port = 3000;
+const port = 3005;
 
 // Configurações do banco de dados
 const dbConfig = {
