86a1d086c1d736ec50fc6c3e1ec9a80a87c891ca
viniciusdev772 <viniciusdev37@gmail.com>
2023-07-28 21:21:27 -0300

    atualizaçãp

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2/promise'); // Usando mysql2 com suporte a promessas
+const NodeCache = require('node-cache');
 const app = express();
 const port = 3005;
 
@@ -20,6 +21,9 @@ const pool = mysql.createPool(dbConfig);
 // Middleware para receber dados JSON em requisições POST
 app.use(express.json());
 
+// Criação do cache em memória com tempo de vida de 1 hora (3.600.000 milissegundos)
+const cache = new NodeCache({ stdTTL: 3600 });
+
 // Rota para receber a chave "text" via POST
 app.post('/api', async (req, res) => {
   const text = req.body.text;
@@ -36,7 +40,7 @@ app.post('/api', async (req, res) => {
       res.status(500).send('Erro ao processar a requisição.');
     }
   } else {
-    res.status(400).send('A chave "text" não foi fornecida na requisição POST  Versão 2.0 da Api.');
+    res.status(400).send('A chave "text" não foi fornecida na requisição POST.');
   }
 });
 
@@ -56,7 +60,7 @@ app.get('/api', async (req, res) => {
       res.status(500).send('Erro ao processar a requisição.');
     }
   } else {
-    res.status(400).send('A chave "text" não foi fornecida na requisição GET Versão 2.0 da Api.');
+    res.status(400).send('A chave "text" não foi fornecida na requisição GET.');
   }
 });
 
@@ -69,24 +73,20 @@ async function verificarPalavrasProibidas(text) {
 
   // Procurar por palavras proibidas no cache em memória primeiro
   const cacheKey = palavras.join('|');
-  if (verificarPalavrasProibidas.cache[cacheKey]) {
-    return verificarPalavrasProibidas.cache[cacheKey];
+  const cachedResult = cache.get(cacheKey);
+  if (cachedResult) {
+    return cachedResult;
   }
 
   const [results] = await pool.query(sqlQuery, [cacheKey]);
   const palavrasProibidas = results.map((row) => row.palavra);
   const quantidadePalavrasProibidas = palavrasProibidas.length;
 
-  // Armazenar resultado em cache por 1 minuto (ajuste conforme necessidade)
-  verificarPalavrasProibidas.cache[cacheKey] = {
-    quantidadePalavrasProibidas,
-    palavrasProibidas,
-  };
+  // Armazenar resultado em cache por 1 hora (3.600.000 milissegundos)
+  cache.set(cacheKey, { quantidadePalavrasProibidas, palavrasProibidas }, 3600);
 
   return { quantidadePalavrasProibidas, palavrasProibidas };
 }
-// Cache em memória com tempo de vida de 1 minuto para resultados
-verificarPalavrasProibidas.cache = {};
 
 // Iniciando o servidor
 app.listen(port, () => {
