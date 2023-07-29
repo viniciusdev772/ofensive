1af63c9b2be039ee4fc42ad80e13b260f32f86e0
viniciusdev772 <viniciusdev37@gmail.com>
2023-07-28 22:02:25 -0300

    up

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise'); // Usando mysql2 com suporte a promessas
+const NodeCache = require('node-cache');
 const app = express();
 const port = 3005;
 
@@ -14,31 +15,22 @@ const dbConfig = {
 //definir titulo
 app.set('title', 'OFENSIVES');
 
-// Criação da conexão com o banco de dados
-const connection = mysql.createConnection(dbConfig);
+// Criação do pool de conexões com o banco de dados
+const pool = mysql.createPool(dbConfig);
 
-// Conectando ao banco de dados
-connection.connect((err) => {
-  if (err) {
-    console.error('Erro ao conectar ao banco de dados:', err);
-    return;
-  }
-  console.log('Conexão com o banco de dados estabelecida com sucesso!');
-});
-
-// Middleware para receber dados JSON em requisições POST
-app.use(express.json());
+// Criação do cache em memória com tempo de vida de 1 hora (3.600.000 milissegundos)
+const cache = new NodeCache({ stdTTL: 3600 });
 
 // Rota para receber a chave "text" via POST
 app.post('/api', async (req, res) => {
   const text = req.body.text;
   if (text) {
-    // Verifica se a palavra é proibida
-    const result = await verificarPalavrasProibidas(text);
-    if (result.quantidadePalavrasProibidas > 0) {
-      res.status(200).json(result);
-    } else {
-      res.status(200).json({ mensagem: `Texto recebido via POST: ${text}` });
+    try {
+      // Verifica se a palavra é proibida
+      const result = await verificarPalavrasProibidas(text);
+      res.status(200).json({ ...result, cached: result.cached });
+    } catch (err) {
+      res.status(500).send('Erro ao processar a requisição.');
     }
   } else {
     res.status(400).send('A chave "text" não foi fornecida na requisição POST.');
@@ -49,12 +41,12 @@ app.post('/api', async (req, res) => {
 app.get('/api', async (req, res) => {
   const text = req.query.text;
   if (text) {
-    // Verifica se a palavra é proibida
-    const result = await verificarPalavrasProibidas(text);
-    if (result.quantidadePalavrasProibidas > 0) {
-      res.status(200).json(result);
-    } else {
-      res.status(200).json({ mensagem: `Texto recebido via GET: ${text}` });
+    try {
+      // Verifica se a palavra é proibida
+      const result = await verificarPalavrasProibidas(text);
+      res.status(200).json({ ...result, cached: result.cached });
+    } catch (err) {
+      res.status(500).send('Erro ao processar a requisição.');
     }
   } else {
     res.status(400).send('A chave "text" não foi fornecida na requisição GET.');
@@ -62,22 +54,27 @@ app.get('/api', async (req, res) => {
 });
 
 // Função para verificar palavras proibidas em um texto
-function verificarPalavrasProibidas(text) {
+async function verificarPalavrasProibidas(text) {
   // Quebra o texto em palavras usando espaço como delimitador
   const palavras = text.split(' ');
 
   const sqlQuery = 'SELECT palavra FROM badwords WHERE palavra REGEXP ?';
-  return new Promise((resolve, reject) => {
-    connection.query(sqlQuery, [palavras.join('|')], (err, results) => {
-      if (err) {
-        reject(err);
-      } else {
-        const palavrasProibidas = results.map((row) => row.palavra);
-        const quantidadePalavrasProibidas = palavrasProibidas.length;
-        resolve({ quantidadePalavrasProibidas, palavrasProibidas });
-      }
-    });
-  });
+
+  // Procurar por palavras proibidas no cache em memória primeiro
+  const cacheKey = palavras.join('|');
+  const cachedResult = cache.get(cacheKey);
+  if (cachedResult) {
+    return { ...cachedResult, cached: true };
+  }
+
+  const [results] = await pool.query(sqlQuery, [cacheKey]);
+  const palavrasProibidas = results.map((row) => row.palavra);
+  const quantidadePalavrasProibidas = palavrasProibidas.length;
+
+  // Armazenar resultado em cache por 1 hora (3.600.000 milissegundos) em memória
+  cache.set(cacheKey, { quantidadePalavrasProibidas, palavrasProibidas, cached: false }, 3600);
+
+  return { quantidadePalavrasProibidas, palavrasProibidas: palavrasProibidas.join(', '), cached: false };
 }
 
 // Iniciando o servidor
