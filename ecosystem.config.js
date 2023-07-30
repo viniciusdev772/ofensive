4391c22a1547df435a122056345683ab7b30bba5
ViniciusDev <viniciusdev37@gmail.com>
2023-07-30 00:40:27 -0300

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: 'my-app', // Nome da aplicação
+      script: 'npm', // Comando que será executado
+      args: 'start', // Argumentos do comando (npm start)
+      autorestart: true, // Reiniciar automaticamente em caso de falha
+      watch: false, // Monitorar alterações nos arquivos (true/false)
+      max_memory_restart: '1G', // Limite máximo de memória para reiniciar o processo
+    },
+  ],
+};
