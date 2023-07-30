9df1bd58285ed0b8b3d3cc0c14214732c52e232a
ViniciusDev <viniciusdev37@gmail.com>
2023-07-30 00:45:59 -0300

    Atualizar o ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'my-app', // Nome da aplicação
       script: 'npm', // Comando que será executado
-      args: 'start', // Argumentos do comando (npm start)
+      args: 'vadia', // Argumentos do comando (npm start)
       autorestart: true, // Reiniciar automaticamente em caso de falha
       watch: false, // Monitorar alterações nos arquivos (true/false)
       max_memory_restart: '1G', // Limite máximo de memória para reiniciar o processo
